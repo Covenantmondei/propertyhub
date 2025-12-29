e61d58e34a4e28ac880bd9ccd60d2c73a96b4ef8
Covenant Monday <covenantmonday863@gmail.com>
2025-12-30 00:17:31 +0100

    changed smtp to brevo

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,3 +1,4 @@
+import httpx
 import os
 from fastapi_mail import ConnectionConfig
 
@@ -17,3 +18,44 @@ conf = ConnectionConfig(
     MAIL_FROM_NAME="RealEstate App",
     # TEMPLATE_FOLDER="./templates"
 )
+
+
+SENDER_EMAIL = os.environ.get("EMAIL_HOST_USER")
+BREVO_API_KEY = os.environ.get("BREVO_API_KEY")
+BREVO_URL = "https://api.brevo.com/v3/smtp/email"
+
+HEADERS = {
+    "accept": "application/json",
+    "api-key": BREVO_API_KEY,
+    "content-type": "application/json",
+}
+
+
+async def send_email(
+    to_email: str,
+    subject: str,
+    html_content: str,
+    sender_email: str = SENDER_EMAIL,
+    sender_name: str = "PropertyHub",
+):
+    payload = {
+        "sender": {
+            "name": "PropertyHub",
+            "email": sender_email,
+        },
+        "to": [
+            {"email": to_email}
+        ],
+        "subject": subject,
+        "htmlContent": html_content,
+    }
+
+    async with httpx.AsyncClient(timeout=10) as client:
+        response = await client.post(
+            BREVO_URL,
+            headers=HEADERS,
+            json=payload,
+        )
+
+    response.raise_for_status()
+    return response.json()
